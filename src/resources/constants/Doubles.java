7a28e395b0811fd0d54770158b5a22efad77867c
Ruslan Ardashev <ardashev120101@gmail.com>
2015-03-13 02:02:35 -0400

    extends constants

--- src/resources/constants/Doubles.java
+++ src/resources/constants/Doubles.java
@@ -3,6 +3,7 @@ package resources.constants;
 public class Doubles {
 
 	public static final double TEN_PERCENT =  0.10;
-	public static final double FIFTY_PERCENT =  0.10;
+	public static final double TWENTY_PERCENT =  0.20;
+	public static final double FIFTY_PERCENT =  0.50;
 	
 }
