1fdce6047714013368631343276bcbe77a129772
Ruslan Ardashev <ardashev120101@gmail.com>
2015-03-13 16:05:22 -0400

    imports

--- src/view/View.java
+++ src/view/View.java
@@ -7,6 +7,8 @@ import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
