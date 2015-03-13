166fa92ea28fb99e7dacabaa3d6d9224e6f10b11
Ruslan Ardashev <ardashev120101@gmail.com>
2015-03-13 01:59:17 -0400

    View sizing progress

--- src/view/View.java
+++ src/view/View.java
@@ -1,5 +1,6 @@
 package view;
 
+import resources.constants.Doubles;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
@@ -9,15 +10,42 @@ public class View {
 	private Scene scene;
 	private Group root;
 	
+	private double width, height;
+	private double tenthWidth;
+	private double halfWidth;
+	
+	// Constructor and helpers
 	public View(double width, double height) {
+		
+		configureSize(width, height);
+		createRootAndScene();
+		
+	}
+	
+	private void createRootAndScene() {
 		
 		root = new Group();
 		scene = new Scene(root, width, height, Color.TAN);
 		
 	}
+	
+	private void configureSize(double width, double height) {
+		
+		this.width = width;
+		this.height = height;
+		
+		tenthWidth = Doubles.TEN_PERCENT * width;
+		halfWidth = Doubles.FIFTY_PERCENT * width;
+		
+	}
 
+	
+	// Getters & Setters
 	public Scene getSceneToDisplay() {
 		return scene;
 	}
+	
+	// All other instance methods
+	
 	
 }
