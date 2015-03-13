0cf941b68f2b0aeffe30dc933aef94026ff09a7f
Ruslan Ardashev <ardashev120101@gmail.com>
2015-03-13 02:02:43 -0400

    adds VBox in View

--- src/view/View.java
+++ src/view/View.java
@@ -3,15 +3,18 @@ package view;
 import resources.constants.Doubles;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
 public class View {
 
+	// JavaFX Instance Variables
 	private Scene scene;
 	private Group root;
+	private VBox vBox;
 	
 	private double width, height;
-	private double tenthWidth;
+	private double twentiethWidth;
 	private double halfWidth;
 	
 	// Constructor and helpers
@@ -19,27 +22,39 @@ public class View {
 		
 		configureSize(width, height);
 		createRootAndScene();
+		createAndAddVBox();
 		
+	}
+	
+	private void configureSize(double width, double height) {
+
+		this.width = width;
+		this.height = height;
+
+		twentiethWidth = Doubles.TWENTY_PERCENT * width;
+		halfWidth = Doubles.FIFTY_PERCENT * width;
+
 	}
 	
 	private void createRootAndScene() {
 		
 		root = new Group();
 		scene = new Scene(root, width, height, Color.TAN);
-		
+		vBox = new VBox();
+		root.getChildren().add(vBox);
+
 	}
-	
-	private void configureSize(double width, double height) {
-		
-		this.width = width;
-		this.height = height;
-		
-		tenthWidth = Doubles.TEN_PERCENT * width;
-		halfWidth = Doubles.FIFTY_PERCENT * width;
-		
+
+	private void createAndAddVBox() {
+
+		vBox = new VBox();
+		root.getChildren().add(vBox);
+
 	}
 
 	
+
+
 	// Getters & Setters
 	public Scene getSceneToDisplay() {
 		return scene;
