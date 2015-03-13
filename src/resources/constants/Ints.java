14fdc59fa0eb75aec815cd03994ec54d69cec560
Ruslan Ardashev <ardashev120101@gmail.com>
2015-03-13 17:57:34 -0400

    extends constants

--- src/resources/constants/Ints.java
+++ src/resources/constants/Ints.java
@@ -2,6 +2,11 @@ package resources.constants;
 
 public class Ints {
 
+	// ButtonBox
 	public static final int BUTTON_OFFSET = 10;
+	
+	// DisplayBox 
+	public static final int RECTANGLE_OFFSET = 3;
+	public static final int NUMBER_RECTANGLES = 7;
 	
 }
