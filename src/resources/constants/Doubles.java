71cf0ac1ef2d22ce918b2bc7160021bd066dd4e1
Ruslan Ardashev <ardashev120101@gmail.com>
2015-03-13 17:29:04 -0400

    adds useful constants to Doubles

--- src/resources/constants/Doubles.java
+++ src/resources/constants/Doubles.java
@@ -2,9 +2,14 @@ package resources.constants;
 
 public class Doubles {
 
+	public static final double FIVE_PERCENT =  0.05;
 	public static final double TEN_PERCENT =  0.10;
 	public static final double TWENTY_PERCENT =  0.20;
+	public static final double FOURTY_PERCENT =  0.40;
 	public static final double FIFTY_PERCENT =  0.50;
+	public static final double SEVENTY_FIVE_PERCENT = 0.75;
+	public static final double EIGHTY_PERCENT = 0.80;
+	public static final double EIGHTY_FIVE_PERCENT = 0.85;
 	public static final double NINETY_PERCENT = 0.90;
 	
 }
