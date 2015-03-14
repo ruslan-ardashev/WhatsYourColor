92854515e04ab117ee4de893e518c635c7fe8ba2
Ruslan Ardashev <ardashev120101@gmail.com>
2015-03-13 23:33:45 -0400

    finding average color in an image

---
+++ src/view/util/AverageColorFinder.java
@@ -0,0 +1,47 @@
+package view.util;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.PixelReader;
+import javafx.scene.paint.Color;
+
+public class AverageColorFinder {
+
+	public static Color findAverageColorFromImage(Image image) {
+		
+		// Obtain PixelReader
+        PixelReader pixelReader = image.getPixelReader();
+        
+        double runningR = 0.0, runningG = 0.0, runningB = 0.0, runningAlpha = 0.0;
+        int pixels = 0;
+        
+        // Determine the color of each pixel in the image
+        for (int readY = 0; readY < image.getHeight(); readY++) {
+        	
+            for (int readX = 0; readX < image.getWidth(); readX++) {
+            	
+                Color color = pixelReader.getColor(readX, readY);
+                
+                if (color.getRed() > 1.0 || color.getBlue() > 1.0 || color.getGreen() > 1.0) {
+                	System.out.println("TOO MUCH");
+                }
+                
+                runningR += color.getRed();
+                runningG += color.getGreen();
+                runningB += color.getBlue();
+                runningAlpha += color.getOpacity();
+                
+                pixels++;
+                
+            }
+            
+        }
+        
+        System.out.println("red: "+ runningR / pixels+", blue: "+ runningB / pixels);
+        
+        return new Color(runningR / pixels, runningG / pixels, runningB / pixels, runningAlpha / pixels);
+		
+	}
+
+	
+	
+}
