6e47a0f1123057b54f3e06d78230f0d4f3ccca16
Ruslan Ardashev <ardashev120101@gmail.com>
2015-03-13 23:42:34 -0400

    adds height to instance and constructor GradientRecFactory

--- src/view/box/util/GradientRectangleFactory.java
+++ src/view/box/util/GradientRectangleFactory.java
@@ -2,6 +2,12 @@ package view.box.util;
 
 public class GradientRectangleFactory {
 
+	private double height;
 	
+	public GradientRectangleFactory(double height) {
+		
+		this.height = height;
+		
+	}
 	
 }
