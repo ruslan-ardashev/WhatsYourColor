a4289f8114894e8e449f46910fd28db0366ecff4
Ruslan Ardashev <ardashev120101@gmail.com>
2015-04-02 10:49:40 -0400

    playing with RestFB

--- src/view/facebookView/FacebookView.java
+++ src/view/facebookView/FacebookView.java
@@ -13,16 +13,26 @@ import view.facebookView.facebookLoginBox.FacebookLoginController;
 public class FacebookView extends View implements FacebookLoginController {
 
 	// Instance Variables
+	// JavaFX
 	private FacebookLoginBox facebookLoginBox;
+	// RestFB
+	private FacebookClient facebookClient;
 	
 	
 	// Constructor & Helpers
 	public FacebookView(double width, double height) {
 		
 		super(width, height);
-		
+
+		instantiateFacebookClient();
 		instantiateFacebookLoginBox();
 		
+		testPrintingUser();
+		
+	}
+	
+	private void instantiateFacebookClient() {
+		this.facebookClient = new DefaultFacebookClient(OAuth.OAuth, Version.VERSION_2_2);
 	}
 	
 	private void instantiateFacebookLoginBox() {
@@ -34,6 +44,15 @@ public class FacebookView extends View implements FacebookLoginController {
 	
 	
 	// All other instance methods
-	
+	private void testPrintingUser() {
+		
+//		User user = facebookClient.fetchObject("me", User.class);
+//		System.out.println("User name: "+user.getName()+", other: "+user.getPicture());
+				
+		Connection<User> photos = facebookClient.fetchConnection("me/photos", User.class,Parameter.with("fields", "data"));
+
+		System.out.println(photos);
+		
+	}
 	
 }
