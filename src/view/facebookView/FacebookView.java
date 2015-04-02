2d3cfce9ec2442d0559b01870f7ef60ee12dbce2
Ruslan Ardashev <ardashev120101@gmail.com>
2015-04-02 10:50:48 -0400

    removes RestFB, transitioning to Nashorn API

--- src/view/facebookView/FacebookView.java
+++ src/view/facebookView/FacebookView.java
@@ -3,9 +3,6 @@ package view.facebookView;
 import javafx.scene.image.ImageView;
 import resources.OAuth.OAuth;
 
-import com.restfb.*;
-import com.restfb.types.User;
-
 import view.View;
 import view.facebookView.facebookLoginBox.FacebookLoginBox;
 import view.facebookView.facebookLoginBox.FacebookLoginController;
@@ -15,8 +12,6 @@ public class FacebookView extends View implements FacebookLoginController {
 	// Instance Variables
 	// JavaFX
 	private FacebookLoginBox facebookLoginBox;
-	// RestFB
-	private FacebookClient facebookClient;
 	
 	
 	// Constructor & Helpers
@@ -24,15 +19,10 @@ public class FacebookView extends View implements FacebookLoginController {
 		
 		super(width, height);
 
-		instantiateFacebookClient();
 		instantiateFacebookLoginBox();
 		
 		testPrintingUser();
 		
-	}
-	
-	private void instantiateFacebookClient() {
-		this.facebookClient = new DefaultFacebookClient(OAuth.OAuth, Version.VERSION_2_2);
 	}
 	
 	private void instantiateFacebookLoginBox() {
@@ -46,12 +36,7 @@ public class FacebookView extends View implements FacebookLoginController {
 	// All other instance methods
 	private void testPrintingUser() {
 		
-//		User user = facebookClient.fetchObject("me", User.class);
-//		System.out.println("User name: "+user.getName()+", other: "+user.getPicture());
-				
-		Connection<User> photos = facebookClient.fetchConnection("me/photos", User.class,Parameter.with("fields", "data"));
-
-		System.out.println(photos);
+		
 		
 	}
 	
